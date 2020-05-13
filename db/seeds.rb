bec1b0ffa9457c4cf118b474b6420316a8def731
Leo Carey-Williams <leocareywilliams@gmail.com>
2020-05-13 15:41:12 +0100

    Adding all files for MVP

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,15 @@
 #
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
+puts 'creating restaurants...'
+rotiking = { name: "Roti King", address: "40 Doric Way, London NW1 1LH", telephone: 02073672516, category: 'Malaysian' }
+dishoom = { name: "Dishoom", address: "7 Boundary St", telephone: 02074203324, category: "Indian" }
+macdonalds = { name: "Macdonalds", address: "Strand, London", telephone: 0205431763, category: "Fast"}
+inamo = { name: "Inamo", address: "134-136 Wardour St", telephone: 0203452453, category: "Japanese" }
+wagamamas = { name: "Wagamamas", address: "Covent Garden, London", telephone: 0205412512, category: 'Japanese' }
+
+[rotiking, dishoom, macdonalds, inamo, wagamamas]. each do |attr|
+  restaurant = Restaurant.create!(attr)
+  puts "Created #{restaurant.name}"
+end
+puts "Completed"
